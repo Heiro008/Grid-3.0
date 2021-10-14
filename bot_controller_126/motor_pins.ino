d889efce368533d4eee7e4ada9174a454bcd85c4
Heiro008 <62615808+Heiro008@users.noreply.github.com>
2021-10-14 22:41:08 +0530

    Add files via upload

---
+++ bot_controller_126/motor_pins.ino
@@ -0,0 +1,66 @@
+struct right_pins{
+  byte front_pos = 14;
+  byte front_gnd = 0;
+  byte rear_pos  = 3;
+  byte rear_gnd  = 15;
+}right;
+struct left_pins{
+  byte front_pos = 4;
+  byte front_gnd = 5 ;
+  byte rear_pos  = 12;
+  byte rear_gnd  = 13;  
+}left;
+void pin_setup(){
+  pinMode(right.front_pos, OUTPUT);
+  pinMode(right.front_gnd, OUTPUT);
+  pinMode(right.rear_pos, OUTPUT);
+  pinMode(right.rear_gnd, OUTPUT);
+  pinMode(left.front_pos, OUTPUT);
+  pinMode(left.front_gnd, OUTPUT);
+  pinMode(left.rear_pos, OUTPUT);
+  pinMode(left.rear_gnd, OUTPUT);
+  pinMode(10, OUTPUT);      //later for servo motor
+  digitalWrite(10 ,LOW);
+}
+void right_move(int value){
+   analogWrite(right.front_pos,value);
+   analogWrite(right.rear_pos,value);
+   analogWrite(right.front_gnd,0);
+   analogWrite(right.rear_gnd,0);
+}
+void left_move(int value){
+   analogWrite(left.front_pos,value);
+   analogWrite(left.rear_pos,value);
+   analogWrite(left.front_gnd,0);
+   analogWrite(left.rear_gnd,0);  
+}
+void left_turn(){
+  right_move(0);
+  left_move(0);
+  Swap(&left.front_pos,&left.front_gnd);
+  Swap(&left.rear_pos,&left.rear_gnd);
+}
+void right_turn(){
+  left_move(0);
+  right_move(0);
+  Swap(&right.front_pos,&right.front_gnd);
+  Swap(&right.rear_pos,&right.rear_gnd);
+}
+void return_back(){
+  //Serial.println("begin swap");
+  Swap(&right.front_pos,&left.front_gnd);
+  Swap(&right.rear_pos,&left.rear_gnd);
+  Swap(&left.front_pos,&right.front_gnd);
+  Swap(&left.rear_pos,&right.rear_gnd);
+}
+
+void Swap(byte *a,byte *b){
+  //Serial.println("swapped");
+  byte temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+void correct_angle(){
+  
+}
